d67c9256e17c6ab7f35f0912a4091d8f6420220b
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-02-08 21:09:31 -0300

    vendo lógica inicial com dart

---
+++ Mobile/exemplo_variaveis_operadores/lib/main.dart
@@ -0,0 +1,14 @@
+
+void main() {
+  //-----===| ATTRIBUTES |===-----//
+  String nome = "João";
+  int idade = 25;
+  double altura = 1.75;
+  bool estudante = true;
+
+  //---=| Imprimindo variáveis
+  print("Nome: $nome");
+  print("Idade: $idade");
+  print("Altura: $altura");
+  print("Estudante: $estudante");
+}
