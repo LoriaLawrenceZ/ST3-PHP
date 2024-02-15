82333f39c38143d1fad50b64257393a553f6d38d
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-02-14 21:15:18 -0300

    tab

---
+++ PHP/index.php
@@ -0,0 +1,15 @@
+<?php
+    integer num1 = 1;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>OI</h1>
+</body>
+</html>
