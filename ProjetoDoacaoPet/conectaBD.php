fdb966ace18de1eeb11786ee290e09c6ae4b7708
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-02-19 21:34:41 -0300

    Fazendo projeto com diogo

--- ProjetoDoacaoPet/conectaBD.php
+++ ProjetoDoacaoPet/conectaBD.php
@@ -1,24 +1,24 @@
 <?php
 // endereco
 // nome do BD
-// usuario
+// admin
 // senha
 $endereco = 'localhost';
 $banco = 'projetodoacaopet';
-$usuario = 'postgres';
+$admin = 'postgres';
 $senha = 'postgres';
 //
 // Comando para criar tabela no terminal:
-// CREATE TABLE usuario(id_usuario SERIAL NOT NULL, nome VARCHAR(255) NOT NULL, data_nascimento VARCHAR(255) NOT NULL, telefone VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, senha VARCHAR(255) NOT NULL, CONSTRAINT pk_id_usuario PRIMARY KEY(id_usuario));
+// CREATE TABLE admin(id_usuario SERIAL NOT NULL, nome VARCHAR(255) NOT NULL, data_nascimento VARCHAR(255) NOT NULL, telefone VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, senha VARCHAR(255) NOT NULL, CONSTRAINT pk_id_usuario PRIMARY KEY(id_usuario), CONSTRAINT uk_email UNIQUE KEY (email));
 //
 try {
     // sgbd:host;port;dbname
-    // usuario
+    // admin
     // senha
     // errmode
     $pdo = new PDO(
         "pgsql:host=$endereco;port=5432;dbname=$banco",
-        $usuario,
+        $admin,
         $senha,
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
     );
